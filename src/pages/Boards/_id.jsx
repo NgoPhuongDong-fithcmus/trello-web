fbd35363b405c7e36532fd52637d90b911dc453a
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-05-19 21:48:55 +0700

    set up axios call api FE

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,15 +3,30 @@ import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import AppBar from '../../components/AppBar/AppBar'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailApi } from '~/apis'
 
 // Board Detail
 function Board() {
+
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = '682aed7da3c4bcfd85b367d1'
+    fetchBoardDetailApi(boardId)
+      .then((board) => {
+        setBoard(board)
+      })
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar/>
-      <BoardBar board={ mockData.board }/>
-      <BoardContent board={ mockData.board }/>
+      <BoardBar board={ board }/>
+      <BoardContent board={ board }/>
     </Container>
   )
 }
 export default Board
+
+// mockData.board
