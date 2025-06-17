d5df7ee05e61399689cd3c6bb596d320c4ed19b0
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-06-17 16:22:33 +0700

    nothing

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'https://trello-api-m3ay.onrender.com'
-// export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'https://trello-api-m3ay.onrender.com'
+export const API_ROOT = 'http://localhost:8017'
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 10
