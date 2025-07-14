22affb459b583a4724e1b4023e2ac3ce8bfa2a87
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-07-14 09:49:13 +0700

    config constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,24 @@
 // export const API_ROOT = 'https://trello-api-m3ay.onrender.com'
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
+
+// export const DEFAULT_PAGE = 1
+// export const DEFAULT_ITEMS_PER_PAGE = 10
+
+// export const CARD_MEMBER_ACTIONS = {
+//   ADD: 'ADD',
+//   REMOVE: 'REMOVE'
+// }
+
+let apiRoot = ''
+
+if (process.env.NODE_ENV === 'production') {
+  apiRoot = 'https://trello-api-m3ay.onrender.com'
+}
+else {
+  apiRoot = 'http://localhost:8017'
+}
+
+export const API_ROOT = apiRoot
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 10
